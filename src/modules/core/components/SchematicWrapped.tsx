0049ec225d2e914ec0857e8afd7da8231f678692
Andres Velez <andres082403@gmail.com>
2025-04-04 18:56:23 -0500

    Add logs schematic

--- src/modules/core/components/SchematicWrapped.tsx
+++ src/modules/core/components/SchematicWrapped.tsx
@@ -35,6 +35,10 @@ export const SchematicWrapped = ({ children }: { children: ReactNode }) => {
   }, [clerkUser, identify, client, user]);
 
   useEffect(() => {
+    console.log({
+      ...client.getFlagCheck(FeatureFlag.MONTHLY_REQUESTS),
+      userId: user?.id,
+    });
     const token = generateToken({
       ...client.getFlagCheck(FeatureFlag.MONTHLY_REQUESTS),
       userId: user?.id,
